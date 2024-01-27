9a42da85e72894407a1ea6b85093fcf29ca2e74c
imranrasulzade <imranrasulzade5@gmail.com>
2024-01-28 03:54:49 +0400

    authentication exception import changed

--- src/main/java/com/matrix/mediconcallapp/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/matrix/mediconcallapp/exception/handler/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import com.matrix.mediconcallapp.exception.NotFoundException;
 import com.matrix.mediconcallapp.model.dto.response.ErrorDetails;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.naming.AuthenticationException;
 import java.util.Date;
 
 @Slf4j
